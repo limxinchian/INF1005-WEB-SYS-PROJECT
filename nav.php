53aa78e022c357975e0246fae93ed7c524ce8c58
BenIsRong <benisrong@gmail.com>
2026-03-23 00:17:24 +0800

    feat: nav bar (not logged in)

---
+++ nav.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-primary d-flex justify-content-between">
+    <a href="index.php" class="navbar-brand ps-3">
+        <img src="assets/images/logo.png" alt="Logo" width="65">
+    </a>
+
+    <form action="" method="get" class="w-70">
+            <input type="text" name="search" placeholder="Butter Chicken Curry" class="form-control bg-white">
+    </form>
+
+    <div class="pe-3 ps-3 d-flex justify-content-between gap-3">
+        <button class="btn btn-secondary w-nav_button"><span class="overpass-mono-light">Login</span></button>
+        <button class="btn btn-secondary w-nav_button"><span class="overpass-mono-light">Register</span></button>
+    </div>
+</nav>
