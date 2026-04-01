5e3cbb1eef53528933d0b7cb9bc6a9d69383794b
xinchian <limxinchian1973@gmail.com>
2026-04-01 13:24:10 +0800

    chores:uncomment the code that check for account status

--- actions/recipes/recipe-delete.php
+++ actions/recipes/recipe-delete.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../config/db.php';
-// require_once __DIR__ . '/../../includes/admin-guard.php';
+require_once __DIR__ . '/../../includes/admin-guard.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('Location: ../../admin/recipes/recipes.php');
