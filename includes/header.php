b7b49cef830f92959c63793dd16aef5db4843b8f
BenIsRong <benisrong@gmail.com>
2026-04-03 10:05:27 +0800

    fix: fix order

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo isset($title) ? $title : 'MealMate'; ?></title>
-<link rel="stylesheet" href="/MealMate/INF1005-WEB-SYS-PROJECT/assets/css/global.css">
 <link rel="stylesheet" href="/MealMate/INF1005-WEB-SYS-PROJECT/assets/css/custom.css">
+<link rel="stylesheet" href="/MealMate/INF1005-WEB-SYS-PROJECT/assets/css/global.css">
 
 <script src="/MealMate/INF1005-WEB-SYS-PROJECT/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
